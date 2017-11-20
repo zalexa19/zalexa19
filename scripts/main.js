1a461db0344b1ff07568c8d0d7ab914bdd0aaa40
alexz <zalexa19@gmail.com>
2017-11-20 14:05:12 -0800

    tesT

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by alexz on 9/29/16.
+ */
+
+$(document).ready(function () {
+
+    $(".burger-nav").on("click", function(){
+
+        $("header nav ul").toggleClass("open");
+    })
+
+
+
+});
